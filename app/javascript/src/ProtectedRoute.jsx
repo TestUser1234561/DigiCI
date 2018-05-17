7d5f01b187ff1cd26e9b0a5980b3c3f2420ef20e
Morgan Fudge <mln_mf@hotmail.com>
2018-05-17 19:35:33 -0230

    Added protected route component

---
+++ app/javascript/src/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+    Route,
+    Redirect,
+} from 'react-router-dom'
+
+export function ProtectedRoute ({component: Component, auth, redirect, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={(props) => auth === true
+                ? <Component {...props} />
+                : <Redirect to={{pathname: redirect}} />}
+        />
+    )
+}
