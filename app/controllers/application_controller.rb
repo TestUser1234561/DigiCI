19ff07653ae27ab2e122d3c75c3fcd7c53493093
Morgan Fudge <mln_mf@hotmail.com>
2018-05-18 03:54:00 -0230

    Removed protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token
+
     def app
         @user = current_user.build.target! if current_user
         render :index
