a750623ebffa5a8b76390dd7938392d352047e33
Morgan Fudge <mln_mf@hotmail.com>
2018-05-19 00:36:46 -0230

    Updated repo reducer Removed unused reducers

--- app/javascript/src/reducers/repo.js
+++ app/javascript/src/reducers/repo.js
@@ -3,24 +3,21 @@ let defaultState = {
     isFetching: false,
     visible: false,
     errors: false,
+    id: false,
     repo: {}
 };
 
 //Actions
 const actions = {
-    finishRepoFetch: 'REPO_FINISH_FETCH',
     toggle: 'REPO_TOGGLE',
     set: 'REPO_SET',
 };
 
 //Action builder
 export const repo = {
-    finishRepoFetch: (repo = [], errors = false) => {
-        return { type: actions.finishRepoFetch, repo: repo, errors: errors }
-    },
-    toggle: (fetch = false) => {return { type: actions.toggle, isFetching: fetch }},
-    set: (repo = {}) => {
-        return { type: actions.set, repo: repo }
+    toggle: (fetch = false, id = false) => {return { type: actions.toggle, isFetching: fetch, id: id }},
+    set: (repo = {}, errors = false) => {
+        return { type: actions.set, repo: repo, errors: errors }
     }
 };
 
@@ -28,12 +25,10 @@ export const repo = {
 export default (state = defaultState, action) => {
     switch(action.type) {
         case actions.toggle:
-            return {...state, visible: !state.visible, isFetching: action.isFetching };
+            let visible = action.id !== state.id ? true : !state.visible;
+            return {...state, visible: visible, isFetching: action.isFetching, id: action.id };
 
         case actions.set:
-            return {...state, repo: action.repo};
-
-        case actions.finishRepoFetch:
             return {...state, isFetching: false, repo: action.repo, errors: action.errors};
 
         default:
