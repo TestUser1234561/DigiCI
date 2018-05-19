4ae4d3f54a858c4f8c2466e1fe6e012ecbbd7dd4
Morgan Fudge <mln_mf@hotmail.com>
2018-05-18 23:01:10 -0230

    Misc. changes

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module DigiCI
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
+    # -- all .rb files in that directory are automatically loaded after loadingRepos
     # the framework and any gems in your application.
   end
 end
