8c1b8a36621223fe2a2e949a96540768ff6906d3
Morgan Fudge <mln_mf@hotmail.com>
2018-05-18 22:55:29 -0230

    Updated new fetch and add methods for repos

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -3,17 +3,30 @@ class RepoController < ApplicationController
 
     def index
         @repos = current_user.repos
-        render json: @repos
+        @repo_array = []
+        @repos.each do |r|
+            @repo_array << r.build
+        end
+        render json: @repo_array
     end
 
     def new
-        pp params
-        @repos = current_user.repos
-        render json: {test: 'hi'}
+        @user = current_user
+        @repo = Repo.create(repo_params)
+        @user.repos << @repo
+        render json: @repo.build
     end
 
     def delete
         @repos = current_user.repos
+        @repos = @repos.map do |r|
+            r.build.target!
+        end
         render json: @repos
+    end
+
+    private
+    def repo_params
+        params.permit(:clone_url, :repo_name)
     end
 end
