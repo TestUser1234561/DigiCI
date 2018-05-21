3cbdf501c49c874bf953d7358a7bbb57dd749655
Morgan Fudge <mln_mf@hotmail.com>
2018-05-21 18:10:37 -0230

    Added stream view functionality for completed tests

--- app/javascript/src/components/dashboard/Stream.jsx
+++ app/javascript/src/components/dashboard/Stream.jsx
@@ -1,10 +1,84 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { actions } from '../../reducers/reducers';
+
+let Line = ({text}) => {
+    return(
+        <span>{text}<br/></span>
+    )
+};
+
+const Chunk = ({chunk}) => {
+    let string, error;
+
+    //Check for error or stdout chunk
+    if(chunk.error) {
+        string = JSON.stringify(chunk.error).replace(/^["{](.*)[}"]$/, '$1').split('\\n');
+        error = true
+    } else {
+        string = JSON.stringify(chunk.stdout).replace(/^"(.*)"$/, '$1').split('\\n');
+    }
+
+
+    //Remove last new line
+    if(string.length > 1) { delete string[string.length - 1] };
+
+    return(
+        <p className={`stream-message ${error ? 'stream-message-error' : null}`}>{string.map((s, i) => <Line key={i} text={s} />)}</p>
+    );
+};
 
 class Stream extends Component {
+
+    componentWillMount() {
+        if (!this.props.stream.stream.history) {
+            if (this.props.stream.status === 1) {
+                //Connect with StreamClient
+            } else {
+                //Fetch from database
+                fetch(`/api/repo/${this.props.repo.id}/stream/${this.props.stream.id}/history`, {
+                    credentials: 'same-origin',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }).then((content) => content.json()).then((data) => {
+                    this.props.updateHistory(data.history);
+                });
+            }
+        }
+    }
+
+    componentDidUpdate(props) {
+        if (props.stream.id !== this.props.stream.id) {
+            if (this.props.stream.status === 1) {
+                //Connect with StreamClient
+            } else {
+                //Fetch from database
+                fetch(`/api/repo/${this.props.repo.id}/stream/${this.props.stream.id}/history`, {
+                    credentials: 'same-origin',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }).then((content) => content.json()).then((data) => {
+                    this.props.updateHistory(data.history);
+                });
+            }
+        }
+    }
+
     render() {
+        let history;
+        if(this.props.stream.stream.history) {
+            history = JSON.parse(this.props.stream.stream.history);
+        }
         return (
             <div id='stream-view'>
+                <i id='stream-connection' className='fas fa-spinner-third fa-spin'/>
+                <div id='stream-container'>
+                    {this.props.stream.stream.history ? history.map((chunk, i) => <Chunk key={i} chunk={chunk} />) : null}
+                </div>
             </div>
         );
     }
@@ -12,11 +86,11 @@ class Stream extends Component {
 
 const mapStateToProps = (state) => ({
     repo: state.repo,
-    user: state.user
+    stream: state.stream
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+    updateHistory: (history) => dispatch(actions.stream.updateHistory(history))
 });
 
 export default connect(
