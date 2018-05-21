4ee2e1de4d3f397771f6055d017ae83aa125756f
Morgan Fudge <mln_mf@hotmail.com>
2018-05-21 20:04:16 -0230

    Reduced stream client code

--- app/javascript/src/StreamClient.js
+++ app/javascript/src/StreamClient.js
@@ -1,36 +1,18 @@
 export default class StreamClient {
-    constructor() {
-        this.events = {};
-        this.start();
-    }
-
-    //Event binder
-    on(event, fn) {
-        this.events[event] = fn;
-    }
-
-    start() {
-        this.ws = new WebSocket('ws:localhost:8080');
+    constructor(ip, callback) {
+        this.ws = new WebSocket(`ws:${ip}:8080`);
         this.ws.binaryType = 'arraybuffer';
         let ws = this.ws;
 
         // Listen for messages
         ws.addEventListener('message', (event) => {
             event = this.read(event.data);
-            console.log(event);
-            if(typeof this.events[event.type] === "function") { this.events[event.type](event) }
-        });
-
-        //Attempt reconnect on close
-        ws.addEventListener('close', () => {
-            setTimeout(() => {
-                this.start();
-            }, 5000)
+            callback(event)
         });
     }
 
-    send(m) {
-        this.ws.send(Buffer.from(JSON.stringify(m)));
+    close() {
+        this.ws.close();
     }
 
     read(m) {
